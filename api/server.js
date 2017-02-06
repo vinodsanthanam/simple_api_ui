11a53b550ef7b1b023ce03326969dc48ad9b7ae2
Vinod Santhanam <vinod.santhanam@thoughtworks.com>
2017-02-06 13:57:59 -0800

    Enabled cors for api, added ui to show datetime

--- api/server.js
+++ api/server.js
@@ -2,6 +2,18 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+}
+
+app.use(allowCrossDomain);
+app.use(express.static(__dirname + '/public'));
+
 app.get('/datetime', function (req, res) {
   res.end(new Date().toString());
 })
