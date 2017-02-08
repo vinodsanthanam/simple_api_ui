318cafaba4ae15ffe8b3bf56564c913dd33f9d61
Vinod Santhanam <vinod.santhanam@thoughtworks.com>
2017-02-07 21:06:10 -0800

    cleaned up dockerfile, package.json to remove config, passing config in runtime to docker in the ./go file

--- ui/server.js
+++ ui/server.js
@@ -8,12 +8,11 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', path.join(__dirname, '/public/views'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.set('port', (process.env.npm_package_config_port));
+app.set('port', (process.env.ui_port));
 app.set('view engine', 'pug');
-app.set('view options', {environment: process.env.npm_package_config_environment || "dev"});
 
 app.use(function(req, res, next){
-  res.locals = {environment: process.env.npm_package_config_environment || "dev"};
+  res.locals = {environment: process.env.ui_environment};
   next();
 });
 
